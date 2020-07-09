d046f7b4860210cb3cb746ab49717136126212ae
wadaryohei <ryohei.w0317wr@icloud.com>
2020-07-09 20:30:09 +0900

    Headコンポーネントを追加

---
+++ src/datas/IndexDatas.ts
@@ -0,0 +1,12 @@
+/**
+ * pages/Indexで使用するデータ
+ * - コンポーネントに渡す静的なデータを記述する
+ */
+
+import { RoutingPath } from '../const/RoutingPath'
+
+export const IndexDatas = {
+  title: 'Paletto',
+  description: '世界にエモーショナルなデザインを。',
+  pathname: RoutingPath.index,
+}
