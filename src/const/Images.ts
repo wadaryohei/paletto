aad1ce39a8e66d770918db47daf7c224ef2852fa
wadaryohei <ryohei.w0317wr@icloud.com>
2020-07-26 17:17:43 +0900

    SVGをstyledComponentに読みこむ用の設定ファイルを追加

---
+++ src/const/Images.ts
@@ -0,0 +1,6 @@
+/**
+ * @memo StyledComponentからSVGを背景画像として読み込み用
+ */
+
+export const SelectBackground =
+  'data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22155%22%20height%3D%2297%22%20viewBox%3D%220%200%20155%2097%22%3E%20%3Cpath%20id%3D%22%E5%A4%9A%E8%A7%92%E5%BD%A2_1%22%20data-name%3D%22%E5%A4%9A%E8%A7%92%E5%BD%A2%201%22%20d%3D%22M69.687%2C9.778a10%2C10%2C0%2C0%2C1%2C15.625%2C0l56.71%2C70.98A10%2C10%2C0%2C0%2C1%2C134.21%2C97H20.789a10%2C10%2C0%2C0%2C1-7.813-16.242Z%22%20transform%3D%22translate(155%2097)%20rotate(180)%22%20fill%3D%22%23262525%22%2F%3E%3C%2Fsvg%3E'
