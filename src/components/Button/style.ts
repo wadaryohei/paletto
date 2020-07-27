81b0a8ba7128e07bf16bf91d6c2a43e57e14366a
wadaryohei <ryohei.w0317wr@icloud.com>
2020-07-27 23:34:15 +0900

    ボタンのスタイルを共通化した

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { ButtonComponent, LinkButtonComponent } from './dom'
 import { Colors } from '../../const/Colors'
 import { Padding } from '../../const/Padding'
@@ -9,7 +9,7 @@ import { hover, unHover } from '../../const/Transition'
 //----------------------------------
 // styledComponent
 //----------------------------------
-export const ButtonStyle = styled(ButtonComponent)`
+export const ButtonBaseStyle = css`
   cursor: pointer;
   padding: ${Padding.p24} ${Padding.p32};
   background-color: ${Colors.midGray};
@@ -74,56 +74,12 @@ export const ButtonStyle = styled(ButtonComponent)`
   }
 `
 
+export const ButtonStyle = styled(ButtonComponent)`
+  ${ButtonBaseStyle};
+`
+
 export const LinkButtonStyle = styled(LinkButtonComponent)`
   button {
-    cursor: pointer;
-    padding: ${Padding.p24} ${Padding.p32};
-    background-color: ${Colors.midGray};
-    border-radius: 99rem;
-    color: ${Colors.white};
-    font-size: ${FontSize.sm};
-    font-family: 'Gilroy-Bold', sans-serif;
-
-    &.primary {
-      transition: ${unHover(200)};
-      background-color: ${Colors.primary};
-
-      &:hover {
-        background-color: ${Colors.gray};
-        transition: ${hover(200)};
-      }
-    }
-
-    &.lg {
-      min-width: 320px;
-      padding: ${Padding.p16} ${Padding.p32};
-      font-weight: ${FontWeight.bold};
-      font-size: ${FontSize.sm};
-    }
-
-    &.md {
-      min-width: 240px;
-      padding: ${Padding.p16} ${Padding.p32};
-      font-weight: ${FontWeight.normal};
-      font-size: ${FontSize.sm};
-    }
-
-    &.sm {
-      min-width: 220px;
-      padding: ${Padding.p16} ${Padding.p32};
-      font-size: ${FontSize.sm};
-    }
-
-    &:disabled {
-      opacity: 0.4;
-      background-color: ${Colors.primary};
-      cursor: default;
-
-      &.primary {
-        &:hover {
-          background-color: ${Colors.primary};
-        }
-      }
-    }
+    ${ButtonBaseStyle};
   }
 `
