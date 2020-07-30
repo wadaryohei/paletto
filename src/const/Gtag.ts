990d61865ce88e6a56d6a7dd83f373e78a3a644d
wadaryohei <ryohei.w0317wr@icloud.com>
2020-07-30 19:01:26 +0900

    netlify側でGoogle Analyticsを取れるかどうか検証するためにGtagコードを削除

--- src/const/Gtag.ts
+++
@@ -1,1 +0,0 @@
-export const GA_TRACKING_ID = 'GTM-WCB2ZJK'
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document'
-import { GA_TRACKING_ID } from '../const/Gtag'
 import { ServerStyleSheet as StyledComponentSheet } from 'styled-components'
 import { ServerStyleSheets as MaterialUiStyleSheets } from '@material-ui/styles'
 
@@ -47,16 +46,10 @@ class MyDocument extends Document {
   }
 
   render() {
-    const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GA_TRACKING_ID}');`
-    const gtmFrame = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-
     return (
       <Html lang="ja">
-        <Head>
-          <script dangerouslySetInnerHTML={{ __html: gtmScript }} />
-        </Head>
+        <Head />
         <body>
-          <noscript dangerouslySetInnerHTML={{ __html: gtmFrame }} />
           <Main />
           <NextScript />
         </body>
